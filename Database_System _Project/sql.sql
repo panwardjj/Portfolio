0d29a8a5a722e3bebff004c1dba4c77bc08fb5f7
panwardjj <panward.jin@dome.tu.ac.th>
2024-02-05 20:50:08 +0700

    edit

--- Database_System _Project/sql.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE tbleuser(
-    id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    fullname varchar(255) NOT NULL,
-    username varchar(255) NOT NULL,
-    useremail varchar(255) NOT NULL,
-    password varchar(255) NOT NULL,
-    regdate timestamp NULL DEFAULT CURRENT_TIMESTAMP
-)ENGINE InnoDB DEFAULT CHARSET=utf8;
